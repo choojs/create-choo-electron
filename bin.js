c99093a2cd85f0cb840f2cdcd85e38926bab7f39
Sholto Maud <sholto.maud@gmail.com>
2018-01-21 16:51:04 +1100

    updated README, and removed bankai@8

--- bin.js
+++ bin.js
@@ -99,7 +99,7 @@ function create (dir, argv) {
     },
     function (done) {
       var pkgs = [
-        'bankai@8',
+        'bankai',
         'electron',
         'electron-builder',
         'electron-builder-squirrel-windows',
